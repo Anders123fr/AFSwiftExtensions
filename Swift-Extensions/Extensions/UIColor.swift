4826ae10bbdc235e92a127953cbda5f84a8697a4
Anders Friis <af@andersfr.dk>
2016-11-25 14:54:14 +0100

    .

--- Swift-Extensions/Extensions/UIColor.swift
+++ Swift-Extensions/Extensions/UIColor.swift
@@ -6,7 +6,7 @@ import UIKit
 
 public extension UIColor {
 
-	static func col(_ r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> UIColor {
+	static func col(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> UIColor {
 		return UIColor(red: r/255, green: g/255, blue: b/255, alpha: a)
 	}
 	
