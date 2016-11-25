23cc9bd8448348bffc6867cd78f8cde756302851
Anders Friis <af@andersfr.dk>
2016-11-25 14:01:09 +0100

    Fix ui alert

--- Swift-Extensions/Extensions/UIAlert.swift
+++ Swift-Extensions/Extensions/UIAlert.swift
@@ -8,37 +8,38 @@
 
 import UIKit
 
-
-// GLOBAL ALERT FUNCTIONS
-
-// Shows a standard alert to the user with a title and an optional message.
-// The alert then has an OK button
-func showAlertInCurrentVC(_ title: String, message: String? = nil) {
-	// Create the alert
-	let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+extension UIAlertView {
+	// GLOBAL ALERT FUNCTIONS
 	
-	// Add the OK button
-	alertController.addAction(UIAlertAction(title: kOK, style: UIAlertActionStyle.default) {
-		action in
-	})
-	
-	alertController.view.tag = getAlertTag()
-	
-	// Show alert that the current app version is too low and an update is required
-	if let vc = findCurrentViewController() {
-		vc.present(alertController, animated: true) {}
+	// Shows a standard alert to the user with a title and an optional message.
+	// The alert then has an OK button
+	static func showInCurrentVC(_ title: String, message: String? = nil) {
+		// Create the alert
+		let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+		
+		// Add the OK button
+		alertController.addAction(UIAlertAction(title: kOK, style: UIAlertActionStyle.default) {
+			action in
+		})
+		
+		alertController.view.tag = getAlertTag()
+		
+		// Show alert that the current app version is too low and an update is required
+		if let vc = findCurrentViewController() {
+			vc.present(alertController, animated: true) {}
+		}
 	}
-}
-
-func getAlertTag() -> Int {
-	return 191756
-}
-
-func isAlertAlreadyShown() -> Bool {
-	if let rv = UIApplication.shared.keyWindow?.subviews.first,
-		let _ = rv.viewWithTag(getAlertTag()) {
-		return true
+	
+	static func getAlertTag() -> Int {
+		return 191756
 	}
-	return false
+	
+	static func isAlertAlreadyShown() -> Bool {
+		if let rv = UIApplication.shared.keyWindow?.subviews.first,
+			let _ = rv.viewWithTag(getAlertTag()) {
+			return true
+		}
+		return false
+	}
+	
 }
-
