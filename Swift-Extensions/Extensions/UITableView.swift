a4b1e382e9ae5881345189b8cbdaf4311e76e931
Anders Friis <af@andersfr.dk>
2016-11-27 14:18:28 +0100

    Add UITableView extension

---
+++ Swift-Extensions/Extensions/UITableView.swift
@@ -0,0 +1,21 @@
+//
+//  UITableView.swift
+//  SwiftExtensions
+//
+//  Created by Anders Friis on 27/11/2016.
+//  Copyright © 2016 cromian. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+	
+	/*
+	Hides separators between empty cells in the table view.
+	Reference: http://stackoverflow.com/questions/1633966/can-i-force-a-uitableview-to-hide-the-separator-between-empty-cells
+	*/
+	func hideEmptyCellSeparators() {
+		tableFooterView = UIView(frame: CGRect.zero)
+	}
+	
+}
